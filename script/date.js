7984577e94be9cbcd8fa94133020c38d3ffbf8d4
Justin Kullu <justink57175@gmail.com>
2020-04-25 07:16:33 +0530

    Create date.js

---
+++ script/date.js
@@ -0,0 +1,5 @@
+var myDate = new Date(); 
+var myDay = myDate.getDate();
+var myMonth = myDate.getMonth();
+var myYear = myDate.getFullYear();
+document.write("Today: "+myDay+"/"+myMonth+"/"+myYear); 
